e0a1e7b5eae8e585abd2edf45075a7d03c39435e
Alexander Hartson <alexander-hartson@live.com>
2019-04-02 21:37:43 -0500

    adding openweathermap API to angular project

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-};
+  production: false,
+  appId: '14642f41fe03b8343a255aeddcc6f680',
+  baseUrl: 'http://',
+}
 
 /*
  * For easier debugging in development mode, you can import the following file
