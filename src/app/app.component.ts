1ba5350d67adae914a9f1689534179b1e6cce13d
Alexander Hartson <alexander-hartson@live.com>
2019-04-14 19:24:58 -0500

    finished app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,15 +6,19 @@ import { Component } from '@angular/core'
   <mat-toolbar color="primary">
     <span>LocalCast Weather</span>
   </mat-toolbar>
-  <div>Your city, your forecast, right now!</div>
+  <div fxLayoutAlign="center">
+    <div class="mat-caption">Your city, your forecast, right now!</div>
+  </div>
   <div fxLayout="row">
     <div fxFlex></div>
-    <div fxFlex="300px">
-      <mat-card>
-        <h2>Current Weather</h2>
+    <mat-card fxFlex="300px">
+      <mat-card-header>
+        <mat-card-title><div class="mat-title">Current Weather</div></mat-card-title>
+      </mat-card-header>
+      <mat-card-content>
         <app-current-weather></app-current-weather>
-      </mat-card>
-    </div>
+      </mat-card-content>
+    </mat-card>
     <div fxFlex></div>
   </div>
   `
